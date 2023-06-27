57a6e5d80194b5ae7a07103200b73d455e148549
i-moonlight <infinite.moonlight0506@gmail.com>
2023-06-28 01:12:53 +0400

    fix user procedures

--- ScienceArchive.DB/procedures/users/delete_user.sql
+++ ScienceArchive.DB/procedures/users/delete_user.sql
@@ -4,12 +4,12 @@ CREATE OR REPLACE PROCEDURE "proc_delete_user"(
 LANGUAGE plpgsql
 AS $$
 BEGIN
-  DELETE FROM "users" as u
-  WHERE u."id" = "p_id";
-  
+  DELETE FROM "users_roles" as ur
+  WHERE ur."user_id" = "p_id";
+
   DELETE FROM "users_auth" as ua
   WHERE ua."user_id" = "p_id";
   
-  DELETE FROM "users_roles" as ur
-  WHERE ur."user_id" = "p_id";
+  DELETE FROM "users" as u
+  WHERE u."id" = "p_id";
 END;$$
--- ScienceArchive.DB/procedures/users/update_user.sql
+++ ScienceArchive.DB/procedures/users/update_user.sql
@@ -9,16 +9,16 @@ CREATE OR REPLACE PROCEDURE "proc_update_user"(
 LANGUAGE plpgsql
 AS $$
 BEGIN
-  UPDATE "users" as u
-  SET
-    "name" = COALESCE("p_name", u."name"),
-    "email" = COALESCE("p_email", u."email")
-  WHERE u."id" = "p_id";
-  
   UPDATE "users_auth" as ua
   SET
     "login" = COALESCE("p_login", ua."login"),
     "password" = COALESCE("p_password", ua."password"),
     "password_salt" = COALESCE("p_password_salt", ua."password_salt")
   WHERE ua."user_id" = "p_id";
+  
+  UPDATE "users" as u
+  SET
+    "name" = COALESCE("p_name", u."name"),
+    "email" = COALESCE("p_email", u."email")
+  WHERE u."id" = "p_id";
 END;$$
