37c0be766019891259f33f7e5033d426ce317d63
i-moonlight <infinite.moonlight0506@gmail.com>
2023-10-07 18:02:40 +0400

    reduced content length of news and articles returned from database

--- ScienceArchive.DB/functions/news/get_all_news.sql
+++ ScienceArchive.DB/functions/news/get_all_news.sql
@@ -14,7 +14,7 @@ BEGIN
     n."id",
     nc."author_id"         AS "authorId",
     n."title",
-    n."body",
+    CASE WHEN length(n."body") > 700 THEN concat(left(n."body", 700), '...') ELSE n."body" END AS "body",
     nc."created_timestamp" AS "creationDate"
   FROM "news"                  AS n
     INNER JOIN "news_creation" AS nc on n.id = nc.news_id;
